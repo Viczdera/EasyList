38cc858b40769073a9372ad461d51705e028e118
Viczdera <vichizsoft@yahoo.com>
2022-07-26 01:03:15 +0100

    listview used

--- lib/products.dart
+++ lib/products.dart
@@ -19,7 +19,7 @@ class Products extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return ListView(
       children: products
           .map(
             (element) => Card(
